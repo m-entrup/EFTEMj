e4868838535aa01f604dcfc1de987a9dc126c6af
Michael Entrup (b. Epping) <entrup@arcor.de>
2015-10-16 18:02:55 +0200

    Starting to port form JavaScript to Java:
    Ported the main method from the JavaScript file.

--- EFTEMj-SR-EELS/src/main/java/de/m_entrup/EFTEMj_SR_EELS/SR_EELS_Import.java
+++ EFTEMj-SR-EELS/src/main/java/de/m_entrup/EFTEMj_SR_EELS/SR_EELS_Import.java
@@ -28,19 +28,89 @@
 
 package de.m_entrup.EFTEMj_SR_EELS;
 
+import java.util.ArrayList;
+
 import de.m_entrup.EFTEMj_lib.EFTEMj_Debug;
 import ij.IJ;
+import ij.Prefs;
 import ij.plugin.PlugIn;
 
 public class SR_EELS_Import implements PlugIn {
 
+	private String databasePath;
+
 	@Override
 	public void run(final String arg) {
-		IJ.showMessage("Das Plugin funktioniert.");
+		/*
+		 * Read path to database from IJ_Prefs.txt or ask the user to set the path.
+		 */
+		IJ.showStatus("Loading path of database from IJ_Prefs.txt.");
+		databasePath = Prefs.get(SR_EELS_PrefsKeys.characterisationDatabasePath
+			.getValue(), null);
+		if (databasePath != null) {
+			databasePath = IJ.getDirectory("Set the path to the database...");
+			if (databasePath != null) return;
+			Prefs.set(SR_EELS_PrefsKeys.characterisationDatabasePath.getValue(),
+				databasePath);
+			Prefs.savePreferences();
+		}
+		/*
+		 *  Step 1
+		 *  Select the folder to import from.
+		 */
+		String path;
+		IJ.showStatus("Loading files for import.");
+		if ((path = IJ.getDirectory("Characterisation files...")) == null) return;
+		/*
+		 *  Step 2
+		 *  Show list of files to select from.
+		 */
+		ArrayList<String> files;
+		if ((files = selectFiles(path)) == null) return;
+		/*
+		 *  Step 3
+		 *  Show a dialog to amend the parameters
+		 */
+		ParameterSet parameters;
+		if ((parameters = getParameters(path)) == null) return;
+		/*
+		 *  Step 4
+		 *  Import files to the database.
+		 */
+		if (parameters.date != null & parameters.SpecMag != null &
+			parameters.QSinK7 != null)
+		{
+			saveFiles(path, files, parameters);
+		}
+		IJ.showStatus("Finished importing from " + path);
+	}
+
+	private ArrayList<String> selectFiles(final String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ParameterSet getParameters(final String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void saveFiles(final String path, final ArrayList<String> files,
+		final ParameterSet parameters)
+	{
+		// TODO Auto-generated method stub
+
 	}
 
 	public static void main(final String[] args) {
 		EFTEMj_Debug.debug(SR_EELS_Import.class);
 	}
 
+	private class ParameterSet {
+
+		String date;
+		String QSinK7;
+		String comment;
+		String SpecMag;
+	}
 }
