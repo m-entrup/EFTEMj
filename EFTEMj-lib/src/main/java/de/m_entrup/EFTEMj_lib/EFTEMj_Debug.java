26bc6e023890ed054841a694fb962735d4699af7
Michael Entrup (b. Epping) <entrup@arcor.de>
2015-10-16 17:43:28 +0200

    Adding EFTEMj-lib v0.1.0.

---
+++ EFTEMj-lib/src/main/java/de/m_entrup/EFTEMj_lib/EFTEMj_Debug.java
@@ -0,0 +1,125 @@
+/**
+ * EFTEMj - Processing of Energy Filtering TEM images with ImageJ
+ *
+ * Copyright (c) 2015, Michael Entrup b. Epping <mail@m-entrup.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.m_entrup.EFTEMj_lib;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.plugin.PlugIn;
+
+/**
+ * This class is used for debugging. The main method runs ImageJ and sets the
+ * plugin directory. There are additional methods for logging.
+ *
+ * @author Michael Entrup b. Epping <mail@m-entrup.de>
+ */
+public class EFTEMj_Debug {
+
+	/**
+	 * Disable all debugging.
+	 */
+	public static final int DEBUG_NONE = 0;
+	/**
+	 * Reduce the debug logging to the most essential information.
+	 */
+	public static final int DEBUG_MINIMAL_LOGGING = 31;
+	/**
+	 * Write all debug logs to a file.
+	 */
+	public static final int DEBUG_LOGGING = 63;
+	/**
+	 * Show all debug logs in the ImageJ log window.
+	 */
+	public static final int DEBUG_IN_APP_LOGGING = 127;
+	/**
+	 * Display images that have only debugging purpose.
+	 */
+	public static final int DEBUG_SHOW_IMAGES = 191;
+	/**
+	 * Don't hide any information.
+	 */
+	public static final int DEBUG_FULL = 255;
+	/**
+	 * The current level of debugging.
+	 */
+	private static int debugLevel = DEBUG_NONE;
+
+	/**
+	 * @return The current level of debugging.
+	 */
+	public static int getDebugLevel() {
+		return debugLevel;
+	}
+
+	/**
+	 * Use the constants defined in {@link EFTEMj_Debug} to set the correct level
+	 * of debugging.
+	 *
+	 * @param debugLevel can be a value between 0 (no debugging) and 255 (full
+	 *          debugging).
+	 */
+	public static void setDebugLevel(final int debugLevel) {
+		EFTEMj_Debug.debugLevel = debugLevel;
+	}
+
+	/**
+	 * Start ImageJ.
+	 *
+	 * @param args Not used
+	 */
+	public static void main(final String[] args) {
+		debug(null);
+	}
+
+	/**
+	 * Start {@link ImageJ} and run a {@link PlugIn}.
+	 *
+	 * @param caller is a class that contains a run method.
+	 */
+	public static void debug(final Class<?> caller) {
+		EFTEMj_Debug.debug(caller, debugLevel);
+	}
+
+	/**
+	 * Start {@link ImageJ} and run a {@link PlugIn}.
+	 *
+	 * @param caller is a class that contains a run method.
+	 * @param debugLevel defines the used level of debugging.
+	 */
+	public static void debug(final Class<?> caller, final int debugLevel) {
+		EFTEMj_Debug.debugLevel = debugLevel;
+		// start ImageJ
+		final ImageJ ij = new ImageJ();
+		System.out.println("Testing with " + ij.getInfo());
+
+		if (caller != null) {
+			// run the plugin
+			IJ.runPlugIn(caller.getName(), "");
+		}
+	}
+}
---
+++ EFTEMj-lib/src/main/java/de/m_entrup/EFTEMj_lib/EFTEMj_Prefs.java
@@ -0,0 +1,103 @@
+/**
+ * EFTEMj - Processing of Energy Filtering TEM images with ImageJ
+ *
+ * Copyright (c) 2015, Michael Entrup b. Epping <mail@m-entrup.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.m_entrup.EFTEMj_lib;
+
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Set;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.Prefs;
+
+/**
+ * A class that handles writing and reading of EFTEMj related properties to
+ * IJ_Prefs.txt.
+ *
+ * @author #auMichael Entrup b. Epping <mail@m-entrup.de>
+ */
+public class EFTEMj_Prefs {
+
+	/**
+	 * <code>EFTEMj.<code> Prefix for all EFTEMj related properties in
+	 * IJ_Prefs.txt.
+	 */
+	public static final String PREFS_PREFIX = "EFTEMj.";
+
+	public static void main(final String[] args) {
+		/*
+		 * start ImageJ
+		 */
+		final ImageJ ij = new ImageJ();
+		System.out.println("Testing with " + ij.getInfo());
+
+		final Properties props = Prefs.getControlPanelProperties();
+		final Set<Object> set = props.keySet();
+		final String searchWord = "dispersion";
+		final ArrayList<String> found = new ArrayList<String>();
+		for (final Object i : set) {
+			final String str = (String) i;
+			if (str.contains("EFTEMj")) {
+				if (str.contains(searchWord)) {
+					found.add(str);
+				}
+			}
+		}
+		IJ.showMessage("EFTEMj keys in IJ_Prefs.txt", "directory: " + Prefs
+			.getPrefsDir() + "\nSize: " + set.size() + "\nEFTEMj keys: " + found
+				.size());
+		for (final String i : found) {
+			IJ.log(i + " = " + props.getProperty(i));
+		}
+	}
+
+	public static ArrayList<String> getAllKeys() {
+		final ArrayList<String> keys = new ArrayList<String>();
+		final Properties props = Prefs.getControlPanelProperties();
+		final Set<Object> set = props.keySet();
+		for (final Object obj : set) {
+			String key = (String) obj;
+			key = key.substring(1, key.length());
+			keys.add(key);
+		}
+		return keys;
+	}
+
+	public static ArrayList<String> getAllKeys(final String searchWord) {
+		final ArrayList<String> keys = getAllKeys();
+		final ArrayList<String> found = new ArrayList<String>();
+		for (final String key : keys) {
+			if (key.contains(searchWord)) {
+				found.add(key);
+			}
+		}
+		return found;
+	}
+
+}
