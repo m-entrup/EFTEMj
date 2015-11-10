da368aa600ca917fba8579d05a78f9fbcbb39d61
Michael Entrup (b. Epping) <mail@m-entrup.de>
2015-11-10 12:08:52 +0100

    Adding StringManipulator.java to EFTEMj.lib:
    The class has a static method to remove the file extension from the file
    name. RegExp is used to perform the task.

---
+++ EFTEMj-lib/src/main/java/de/m_entrup/EFTEMj_lib/tools/StringManipulator.java
@@ -0,0 +1,49 @@
+/**
+ * EFTEMj - Processing of Energy Filtering TEM images with ImageJ
+ *
+ * Copyright (c) 2015, Michael Entrup b. Epping <mail@m-entrup.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.m_entrup.EFTEMj_lib.tools;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Michael Entrup b. Epping
+ */
+public class StringManipulator {
+
+	public static String removeExtensionFromTitle(final String title) {
+		final String pattern = "(.*)\\.\\w{2,3}$";
+		final Pattern r = Pattern.compile(pattern);
+		final Matcher m = r.matcher(title);
+		if (m.matches()) {
+			return m.group(1);
+		}
+		return title;
+	}
+
+}
