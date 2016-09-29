c3c31ff5546fd462470cce0e94a7da3253b55668
Michael Entrup <mail@m-entrup.de>
2016-09-29 17:59:23 +0200

    Adding new versions of drift correction:
    These versions compare image by image.

---
+++ EFTEMj-pyScripts/src/main/resources/scripts/Plugins/EFTEMj/Drift_Correction/Correct_drift_(SIFT_linear).py
@@ -0,0 +1,28 @@
+'''
+file:       Correct_drift_(SIFT_linear).py
+author:     Michael Entrup b. Epping (michael.entrup@wwu.de)
+version:    20160929
+info:       A script that corrects the drift between any number of images.
+            Scale-invariant feature transform (SIFT) is used for drift detection.
+            The images are not changed. A stack is created that holds the corrected images.
+'''
+
+from __future__ import with_statement, division
+
+from EFTEMj_pyLib import CorrectDrift as drift
+from EFTEMj_pyLib import Tools as tools
+
+
+def run_script():
+    '''Function to be run when this file is used as a script'''
+    images = tools.get_images()
+    if not images:
+        return
+    elif len(images) >= 2:
+        corrected_stack = drift.get_corrected_stack_linear(images, 'SIFT')
+        corrected_stack.copyScale(images[0])
+        corrected_stack.show()
+
+
+if __name__ == '__main__':
+    run_script()
