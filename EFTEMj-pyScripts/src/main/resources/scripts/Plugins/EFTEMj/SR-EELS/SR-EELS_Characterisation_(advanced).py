05321b4deff3fcc86ad97e026a732564e5d9d615
Michael Entrup (b. Epping) <mail@m-entrup.de>
2016-12-16 15:32:38 +0100

    Adding more parameters to SR-EELS_Characterisation_(advanced).py:
    It is now possible to configure the characterisation itself.
    Until now it was only possible to define how and were to store the results.

--- EFTEMj-pyScripts/src/main/resources/scripts/Plugins/EFTEMj/SR-EELS/SR-EELS_Characterisation_(advanced).py
+++ EFTEMj-pyScripts/src/main/resources/scripts/Plugins/EFTEMj/SR-EELS/SR-EELS_Characterisation_(advanced).py
@@ -1,8 +1,15 @@
 '''
+@Integer(label='Step size', value='64') stepSize
+@Integer(label='Lower energy border', value='128') energyBorderLow
+@Integer(label='Upper energy border', value='128') energyBorderHigh
+@Float(label='Energy position', value='0.5') energyPosition
+@Integer(label='Polynominal order', value='3') polynomialOrder
+@Boolean(label='Use thresholding', value=True) useThresholding
+@String(label='Threshold method', choices={'Li', 'Default', 'Huang', 'Intermodes', 'IsoData', 'Otsu'}, value='Li') threshold
 @File(label='Select input directory', style='directory', description='An directory containing images for the SR-EELS characterisation.') in_dir
+@Boolean(label='Use custom output directory', value='False') use_custom_output
 @File(label='Select output directory', style='directory', description='An directory to safe the results of the SR-EELS characterisation.') out_dir
-@String(label='Rotate images', choices = {'left', 'right', 'none'}, description='Rotation is needed if the energy dispersive axis is not the x axis.') do_rotate
-@Boolean(label='Create subdir', description='Create a directory for the results. The name includes the characterisation parameters.') create_subdir
+@String(label='Rotate images', choices = {'left', 'right', 'none'}, value='none', description='Rotation is needed if the energy dispersive axis is not the x axis.') do_rotate
 @Boolean(label='Create JPEG', description='The analysed images are saved as JPEGs with the detected borders marked.') create_jpeg
 @Boolean(label='Create plots', description='Plots are created that sumarize the characterisation results.') create_plots
 @Boolean(label='Create TIFF', description='Save the results as a tiff dataset. These files are used by the SR-EEL correction.') create_tiff
@@ -10,10 +17,12 @@
 
 file:       SR-EELS_Characterisation_(advanced).py
 author:     Michael Entrup b. Epping (michael.entrup@wwu.de)
-version:    20161021
+version:    20161216
 info:       A script that runs a characterisation on SR-EELS images.
             It is designed to extend the functionality of the SR_EELS_CharacterisationPlugin.
 '''
+import math
+
 from java.io import File
 
 from de.m_entrup.EFTEMj_SR_EELS.characterisation import SR_EELS_CharacterisationPlugin
@@ -22,26 +31,42 @@ from de.m_entrup.EFTEMj_SR_EELS.characterisation import SR_EELS_Characterisation
 
 from os import listdir
 
-def main():
+def get_task():
     task =  SR_EELS_CharacterisationPlugin()
     task.setRotation(do_rotate)
+    return task
+
+def get_settings():
     settings = SR_EELS_CharacterisationSettings()
+    settings.stepSize = stepSize
+    settings.filterRadius = math.sqrt(stepSize)
+    settings.energyBorderLow = energyBorderLow
+    settings.energyBorderHigh = energyBorderHigh
+    settings.energyPosition = energyPosition
+    settings.polynomialOrder = polynomialOrder
+    settings.useThresholding = useThresholding
+    settings.threshold = threshold
     settings.path = in_dir
+    return settings
+
+def main():
+    task =  get_task()
+    settings = get_settings()
     settings.images = task.getImages(settings)
     task.performCharacterisation(settings)
     flags = 0
     if create_jpeg:
-     flags += SR_EELS_CharacterisationResults.JPEG
+        flags += SR_EELS_CharacterisationResults.JPEG
     if create_plots:
-     flags += SR_EELS_CharacterisationResults.PLOTS
+        flags += SR_EELS_CharacterisationResults.PLOTS
     if create_tiff:
-     flags += SR_EELS_CharacterisationResults.TIFF
+        flags += SR_EELS_CharacterisationResults.TIFF
     if create_csv:
-     flags += SR_EELS_CharacterisationResults.CSV
-    if create_subdir:
+        flags += SR_EELS_CharacterisationResults.CSV
+    if use_custom_output:
         final_out_dir = File(out_dir, settings.toString())
     else:
-        final_out_dir = out_dir
+        final_out_dir = File(settings.path, settings.toString())
     task.saveResults(final_out_dir, flags)
 
 
