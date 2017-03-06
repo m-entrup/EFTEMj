63396d30d1531fa6e1fd5332dcf1c4a852b3ec41
Michael Entrup (b. Epping) <mail@m-entrup.de>
2017-03-06 14:08:53 +0100

    Enhancing EELS_Calibration.py:
    Adding missing doc strings.
    Renaming variables to fit pep8.

--- EFTEMj-pyScripts/src/main/resources/scripts/Plugins/EFTEMj/EELS/EELS_Calibration.py
+++ EFTEMj-pyScripts/src/main/resources/scripts/Plugins/EFTEMj/EELS/EELS_Calibration.py
@@ -1,27 +1,50 @@
 """
-@File(label='Input directory', style='directory') srcDir
-@boolean(label='Use fast mode', description='calc in fourier space') fast_mode
-@boolean(label='Debug mode') DEBUG
+@File(label='Input directory', style='directory') SRC_DIR
+@boolean(label='Use fast mode', description='calc in fourier space', value=true) FAST_MODE
+@boolean(label='Debug mode', value=false) DEBUG
 
 file:       EELS_Calibration.py
 author:     Michael Entrup b. Epping (michael.entrup@wwu.de)
 version:    20170123
 info:       A script to get the energy dispersion from a series of spectra.
 """
+# pylint: disable-msg=C0103
+# pylint: enable-msg=C0103
+
 
 from __future__ import with_statement, division
 
 import csv
 import os
-from exceptions import ValueError
+from collections import deque
 
+# pylint: disable-msg=E0401
 from ij import IJ
 from ij.gui import Plot
 from ij.gui import ProfilePlot
+from ij.measure import CurveFitter
+from org.apache.commons.math3.stat.correlation import PearsonsCorrelation
+from org.apache.commons.math3.transform import FastFourierTransformer
+from org.apache.commons.math3.transform import DftNormalization as norm
+from org.apache.commons.math3.transform import TransformType as trans_type
+# pylint: enable-msg=E0401
 
-errors = [] # Save errors to this list and desplay them at the end of the script.
+# pylint: disable-msg=E0602
+SRC_DIR = SRC_DIR
+FAST_MODE = FAST_MODE
+DEBUG = DEBUG
+# pylint: enable-msg=E0602
+
+# Save errors to this list and desplay them at the end of the script.
+ERRORS = []
+
 
 class Spectrum:
+    """A class that represents an EEL Spectrum.
+    There are methods to create a Spectrum from a file.
+    And methods to correlate two spectra with each other.
+    Finally the spectrum can be plotted using IJs Plot class.
+    """
 
     @classmethod
     def get_spectrum_csv(cls, csv_file):
@@ -31,21 +54,22 @@ class Spectrum:
         """
         spectrum = []
         with open(csv_file, 'rb') as csvfile:
-            delim=','
+            delim = ','
             if csv_file.endswith('.xls'):
-                delim='\t'
+                delim = '\t'
             reader = csv.reader(csvfile, delimiter=delim)
             index = 0
             for row in reader:
                 try:
                     spectrum.append({
-                                     'x': index,
-                                     'dE': float(row[0]),
-                                     'y': float(row[1])
-                                    })
+                        'x': index,
+                        'dE': float(row[0]),
+                        'y': float(row[1])
+                    })
                     index += 1
                 except ValueError:
-                    errors.append('%s: Skipping this row: %s' % (os.path.basename(csv_file),row))
+                    ERRORS.append('%s: Skipping this row: %s' %
+                                  (os.path.basename(csv_file), row))
         return spectrum
 
     @classmethod
@@ -61,13 +85,14 @@ class Spectrum:
             for row in reader:
                 try:
                     spectrum.append({
-                                     'x': index,
-                                     'dE': float(row[0]),
-                                     'y': float(row[1])
-                                    })
+                        'x': index,
+                        'dE': float(row[0]),
+                        'y': float(row[1])
+                    })
                     index += 1
                 except ValueError:
-                    errors.append('%s: Skipping this row: %s' % (os.path.basename(msa_file),row))
+                    ERRORS.append('%s: Skipping this row: %s' %
+                                  (os.path.basename(msa_file), row))
         return spectrum
 
     @classmethod
@@ -80,14 +105,14 @@ class Spectrum:
         IJ.run(imp, "Select All", "")
         profiler = ProfilePlot(imp)
         plot = profiler.getPlot()
-        ys = plot.getYValues()
+        y_values = plot.getYValues()
         spectrum = []
         index = 0
-        for y in ys:
+        for y_value in y_values:
             spectrum.append({
-                             'x': index,
-                             'y': y
-                            })
+                'x': index,
+                'y': y_value
+            })
             index += 1
         return spectrum
 
@@ -107,109 +132,123 @@ class Spectrum:
         The rest of the file name is ignored.
         '''
         import re
-        pattern = '.*[^\d\.]((?:\d+[\.,])?\\d+)eV.*'
-        m = re.match(pattern, file_path)
-        loss = float(m.group(1))
+        pattern = re.compile('.*[^\\d\\.]((?:\\d+[\\.,])?\\d+)eV.*')
+        match = pattern.match(file_path)
+        loss = float(match.group(1))
         return cls(spectrum, loss)
 
     def __init__(self, spectrum, loss):
         self.spec = spectrum
 
         self.loss = loss
-        self.xs = [item['x'] for item in spectrum]
-        self.ys = [item['y'] for item in spectrum]
+        self.x_values = [item['x'] for item in spectrum]
+        self.y_values = [item['y'] for item in spectrum]
 
     def plot(self):
-        plot =  Plot('Spectrum %deV' % self.loss,
-                     'Position [px]',
-                     'Intensity [a.u.]',
-                     self.xs, self.ys
-                    )
+        """Plot this spectrum by using IJs Plot class.
+        """
+        plot = Plot('Spectrum %deV' % self.loss,
+                    'Position [px]',
+                    'Intensity [a.u.]',
+                    self.x_values, self.y_values
+                   )
         plot.show()
 
     def crosscorrelation(self, spectrum):
         """ Calculate the crosscorrelation of the instance with a given Spectrum.
         The result is again an instance of Spectrum.
         There are to modes to use:
-        1. 'fast_mode' runs in fourier space.
+        1. 'FAST_MODE' runs in fourier space.
         2. The second mode runs without fouriertransform.
         """
-        if fast_mode:
+        if FAST_MODE:
             '''old version
-            from org.apache.commons.math3.util import MathArrays
-            reverse = spectrum.ys[::-1]
-            corr = MathArrays.convolve(self.ys, reverse)
+            from org.apache.commons.math3.util import MathArray_values
+            reverse = spectrum.y_values[::-1]
+            corr = MathArray_values.convolve(self.y_values, reverse)
             '''
-            from org.apache.commons.math3.transform import FastFourierTransformer
-            from org.apache.commons.math3.transform import DftNormalization as norm
-            from org.apache.commons.math3.transform import TransformType as trans_type
             transformer = FastFourierTransformer(norm.STANDARD)
-            fft1 = transformer.transform(self.ys, trans_type.FORWARD)
-            fft2 = transformer.transform(spectrum.ys, trans_type.FORWARD)
+            fft1 = transformer.transform(self.y_values, trans_type.FORWARD)
+            fft2 = transformer.transform(spectrum.y_values, trans_type.FORWARD)
             fft2c = [val.conjugate() for val in fft2]
             corr_fft = [val1.multiply(val2) for val1, val2 in zip(fft1, fft2c)]
             corr_c = transformer.transform(corr_fft, trans_type.INVERSE)
             corr = [val.getReal() for val in corr_c]
         else:
-            from collections import deque
-            from org.apache.commons.math3.stat.correlation import PearsonsCorrelation
             correlator = PearsonsCorrelation()
             corr = []
-            shifted = deque(spectrum.ys)
-            for i in range(len(self.ys)):
-                corr.append(correlator.correlation(self.ys, list(shifted)))
+            shifted = deque(spectrum.y_values)
+            for _ in range(len(self.y_values)):
+                corr.append(correlator.correlation(
+                    self.y_values, list(shifted)))
                 shifted.rotate(1)
-        new_spec = [{ 'x': x, 'y': y } for x, y in zip(self.xs, corr)]
+        new_spec = [{'x': x, 'y': y} for x, y in zip(self.x_values, corr)]
         return Spectrum(new_spec, self.loss - spectrum.loss)
+
 
 def pos_of(values, method):
     """Find the position of a value that is calculated by the given method.
     """
-    m = method(values)
-    pos = [i for i, j in enumerate(values) if j == m]
+    result = method(values)
+    pos = [i for i, j in enumerate(values) if j == result]
     if len(pos) > 1:
-        print('Warning: The %s of the list is not distinct.' (method,))
+        print('Warning: The %s of the list is not distinct.' % (method,))
     return pos[0]
+
 
 def get_shift(spectra):
     """ Use crosscorrelation do determine the shoft between two spectra.
-    The results are two arrays that contain the shift in eV (from title) and in px (from crosscorrelation).
+    The results are two array_values that contain the shift in eV (from title)
+    and in px (from crosscorrelation).
     """
     ref_pos = int(round(len(spectra) / 2)) - 1
     ref = spectra[ref_pos]
     if DEBUG:
         IJ.log('Energy loss of the reference spectrum is %deV' % ref.loss)
-    correlations = [ref.crosscorrelation(spec) for spec in spectra]
+    steps = len(spectra)
+    correlations = []
+    for i, spec in enumerate(spectra):
+        IJ.showProgress(i / steps)
+        correlations.append(ref.crosscorrelation(spec))
+    IJ.showProgress(1)
     if DEBUG:
         for corr in correlations:
             corr.plot()
-    xs = [item.loss for item in correlations]
-    ys = [pos_of(item.ys, max) if item.loss <= 0 else -(len(item.ys) - pos_of(item.ys, max)) for item in correlations]
-    xs = xs
-    ys = ys
-    return xs, ys
+    x_values = [item.loss for item in correlations]
+    y_values = [pos_of(item.y_values, max)
+                if item.loss <= 0
+                else -(len(item.y_values) - pos_of(item.y_values, max))
+                for item in correlations
+               ]
+    return x_values, y_values
 
-def get_lin_fit(xs, ys):
+
+def get_lin_fit(x_values, y_values):
     """Calculate a linear fit for the given values and return it.
     """
-    from ij.measure import CurveFitter
-    fitter = CurveFitter(xs, ys)
+    fitter = CurveFitter(x_values, y_values)
     fitter.doFit(CurveFitter.STRAIGHT_LINE)
     return fitter
 
-def error_of_dispersion(fit, xs):
+
+def error_of_dispersion(fit, x_values):
     """ Calculate the statistical error of the dispersion.
+    See "Fehleranalyse" by John R. Taylor at p. 38f.
     """
-    r2 = [val**2 for val in fit.getResiduals()]
-    s_y = sum(r2) / (len(r2) - 2)
-    x2s = [val**2 for val in xs]
-    dm = s_y**2 * len(r2) / (len(r2) * sum(x2s) - sum(xs)**2)
-    return dm / fit.getParams()[1]**2
+    r_squares = [val**2 for val in fit.getResiduals()]
+    s_y = sum(r_squares) / (len(r_squares) - 2)
+    x_squares = [val**2 for val in x_values]
+    delta_m = s_y**2 * len(r_squares) / (len(r_squares)
+                                         * sum(x_squares) - sum(x_values)**2)
+    return delta_m / fit.getParams()[1]**2
+
 
 def run_script():
+    """Function to be run when this file is used as a script
+    """
     files = []
-    for item in os.listdir(srcDir.getAbsolutePath()):
-        files.append(os.path.join(srcDir.getAbsolutePath(), str(item)))
+    for item in os.listdir(SRC_DIR.getAbsolutePath()):
+        files.append(os.path.join(SRC_DIR.getAbsolutePath(), str(item)))
     if len(files) <= 1:
         return
     spectra = [Spectrum.from_file(file_path) for file_path in files]
@@ -220,24 +259,33 @@ def run_script():
     if DEBUG:
         for spec in spectra:
             spec.plot()
-    plot =  Plot('EELS-Cal of %s' % os.path.basename(srcDir.getAbsolutePath()),
-                 'Energy loss difference [eV]',
-                 'Shift [px]'
-                )
-    xs, ys = get_shift(spectra)
-    plot.addPoints(xs, ys, Plot.CROSS)
-    fit = get_lin_fit(xs, ys)
-    x_fit = range(min(xs), max(xs) + 1)
+    plot = Plot('EELS-Cal of %s' % os.path.basename(SRC_DIR.getAbsolutePath()),
+                'Energy loss difference [eV]',
+                'Shift [px]'
+               )
+    x_values, y_values = get_shift(spectra)
+    plot.addPoints(x_values, y_values, Plot.CROSS)
+    fit = get_lin_fit(x_values, y_values)
+    x_fit = range(int(min(x_values)), int(max(x_values)) + 1)
     y_fit = [fit.f(x) for x in x_fit]
     plot.addPoints(x_fit, y_fit, Plot.LINE)
-    plot.addPoints([0], [0], Plot.LINE) # This is necessary to get a new row at the legend
-    plot.addLegend('Measured shift\ng(x) = %f x + %f\nr^2 = %f' % (fit.getParams()[1], fit.getParams()[0], fit.getRSquared()))
-    plot.addLabel(0.6, 0.4, 'dispersion = %feV/px\n uncertainty: %feV/px\nGatan dispersion: %f' % (-1/fit.getParams()[1],error_of_dispersion(fit, xs), -15 * fit.getParams()[1]))
+    # This is necessary to get a new row at the legend
+    plot.addPoints([0], [0], Plot.LINE)
+    plot.addLegend('Measured shift\ng(x) = %f x + %f\nr^2 = %f' %
+                   (fit.getParams()[1], fit.getParams()[0], fit.getRSquared()))
+    plot.addLabel(0.6,
+                  0.4,
+                  'dispersion = %feV/px\n uncertainty: %feV/px\nGatan dispersion: %f' %
+                  (-1 / fit.getParams()[1],
+                   error_of_dispersion(fit, x_values),
+                   -15 * fit.getParams()[1]
+                  )
+                 )
     plot.show()
     if DEBUG:
-        if len(errors) >= 1:
+        if len(ERRORS) >= 1:
             IJ.log('Errors durring script execution:')
-            for error in errors:
+            for error in ERRORS:
                 IJ.log(error)
 
 if __name__ == '__main__':
